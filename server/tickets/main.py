8c2278b219a7d5afe043a4b938710b54ceeac853
Jonah Hess <Jonahhessdev@gmail.com>
2026-08-31 18:09:04 +0300

    gave redis a lifespan in app

--- server/tickets/main.py
+++ server/tickets/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 
+from tickets.src import redis_client
 from tickets.src.error_handlers import register_exception_handlers
 from tickets.src.router import router as tickets_router
 
-tickets = FastAPI(title="Tickets")
+tickets = FastAPI(title="Tickets", lifespan=redis_client.lifespan)
 
 register_exception_handlers(tickets)
 
