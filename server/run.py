345149e5f83ea31bc75360ab6fd1d6827e21aaf2
Jonah Hess <Jonahhessdev@gmail.com>
2026-09-01 13:08:54 +0300

    lint

--- server/run.py
+++ server/run.py
@@ -3,4 +3,5 @@ import uvicorn
 
 if __name__ == "__main__":
     # This automatically handles the path tracking from the server/ root
-    uvicorn.run("main_site.main:app", host="127.0.0.1", port=8000, reload=True, log_level="debug")
+    uvicorn.run("main_site.main:app", host="127.0.0.1", port=8000, reload=True, 
+                log_level="debug")
