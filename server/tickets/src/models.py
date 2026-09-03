0b9d06d72c01f585fc74f6c9ea2ddfd262c16163
Jonah Hess <Jonahhessdev@gmail.com>
2026-09-03 23:43:28 +0300

    added screening seat

--- server/tickets/src/models.py
+++ server/tickets/src/models.py
@@ -114,3 +114,25 @@ class Seat(Base):
     # Relationships
     auditorium: Mapped[Auditorium] = relationship(
         "Auditorium", back_populates="seats")
+
+class ScreeningSeat(Base):
+    __tablename__ = "screening_seats"
+    __table_args__ = {"extend_existing": True}
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    screening_id: Mapped[int] = mapped_column(
+        Integer,
+        ForeignKey("screenings.id", ondelete="CASCADE"),
+        nullable=False,
+    )
+    seat_id: Mapped[int] = mapped_column(
+        Integer,
+        ForeignKey("seats.id", ondelete="CASCADE"),
+        nullable=False,
+    )
+    is_taken: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime,
+        nullable=False,
+        server_default=text("CURRENT_TIMESTAMP"),
+    )
