091abcfc78cb273d7afc54cb7a6e0e162e48ac71
Jonah Hess <Jonahhessdev@gmail.com>
2026-09-06 23:25:24 +0300

    server cmponent

--- client/user-site/app/tickets/page.tsx
+++ client/user-site/app/tickets/page.tsx
@@ -1,38 +1,31 @@
-"use client";
+import { ticketsApi } from "@/lib/api";
+import Link from "next/link";
+  
 
-import { useEffect, useState } from "react";
+export default async function TicketsPage() {
+    const { data: tickets, error: ticketsError } = await ticketsApi.GET('/api/v1/tickets/purchases')
 
-import { ticketsApiClient } from "@/lib/tickets-api-client";
-import type { components } from "@/types/tickets-schema";
-
-type Ticket = components["schemas"]["TicketResponse"];
-
-export default function TicketsPage() {
-  const [tickets, setTickets] = useState<Ticket[]>([]);
-  const [hasLoaded, setHasLoaded] = useState(false);
-
-  useEffect(() => {
-    async function loadTickets() {
-      const { data } = await ticketsApiClient.GET("/tickets/all");
-      setTickets(data ?? []);
-      setHasLoaded(true);
-    }
-
-    void loadTickets();
-  }, []);
-
-  if (!hasLoaded || tickets.length === 0) {
-    return <div>No tickets found.</div>;
+  if (ticketsError || !tickets || tickets.length === 0) {
+    return <div>
+      <p>No tickets found.</p>
+      To purchase tickets, visit the page: <Link href="/screenings">Screenings</Link>.
+    </div>;
   }
 
   return (
-    <div id="tickets-list">
-      {tickets.map((ticket) => (
-        <div key={ticket.id} className="mb-4">
-          <h1 className="text-2xl font-bold">{ticket.email}</h1>
-          <p className="text-gray-600">{ticket.receipt_number}</p>
+      <main>
+        <h1>Tickets</h1>
+        <p>Browse your ticket purchases.</p>
+        <div id="tickets-list">
+          {tickets.map((ticket) => (
+            <div key={ticket.id} className="mb-4">
+              <Link href={`/tickets/${ticket.id}`}>
+                <h2 className="text-2xl font-bold">{ticket.email}</h2>
+                <p className="text-gray-600">{ticket.receipt_number}</p>
+              </Link>
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
+      </main>
   );
 }
